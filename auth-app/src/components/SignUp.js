7f3ca9a840e5b034336d4b5cd0c16e4ddcc9e650
chrispetito <christopherpetito@gmail.com>
2019-06-14 09:26:59 -0700

    add signup component

---
+++ auth-app/src/components/SignUp.js
@@ -0,0 +1,54 @@
+import React from "react";
+import axios from "axios";
+
+export default class SignUp extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <form>
+          <input
+            required
+            placeholder="username"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChanges}
+          />
+          <input
+            required
+            placeholder="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChanges}
+          />
+        </form>
+      </div>
+    );
+  }
+
+  handleChanges = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  signup = event => {
+    event.preventDefault();
+    const endpoint = `http://localhost:3300/api/register`;
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        localStorage.setItem("jwt", res.data.token);
+        this.props.hisory.push("/api/jokes");
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+}
