b319f4fd3ef1779011e6ec5503a0662326e8ef1d
chrispetito <christopherpetito@gmail.com>
2019-06-14 09:39:05 -0700

    add requireAuth

---
+++ auth-app/src/auth/requireAuth.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const token = localStorage.getItem("jwt");
+
+export default function(Component) {
+  return class Authenticated extends React.Component {
+    render() {
+      const notLoggedIn = <div>Please log in to view jokes.</div>;
+      return <>{token ? <Component {...this.props} /> : notLoggedIn}</>;
+    }
+  };
+}
