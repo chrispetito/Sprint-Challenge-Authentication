ec67c000ad916a107e22edfd05f7eede05aeefd4
chrispetito <christopherpetito@gmail.com>
2019-06-14 10:11:26 -0700

    style login form

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import { NavLink, Route, withRouter } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { NavLink, Route, withRouter } from "react-router-dom";
+import "./App.css";
 
-import Login from './components/Login'
-import JokeList from './components/JokeList'
-import SignUp from './components/SignUp'
+import Login from "./components/Login";
+import JokeList from "./components/JokeList";
+import SignUp from "./components/SignUp";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <div className='page-header'>Authentication Sprint Challenge</div>
+        <header className="App-header">
+          <nav>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/jokes">Jokes</NavLink>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </nav>
+          <button onClick={this.logout}>Log Out</button>
+        </header>
+        <Route path="/login" component={Login} />
+        <Route path="/jokes" component={JokeList} />
+        <Route path="/signup" component={SignUp} />
+      </div>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <nav>
-        <NavLink to='/login'>Login</NavLink>
-        <NavLink to='/jokes'>Jokes</NavLink>
-        <NavLink to='/signup'>Sign Up</NavLink>
-      </nav>
-
-      </header>
-      <Route path='/login' component={Login}></Route>
-      <Route path='/jokes' component={JokeList}></Route>
-      <Route path='/signup' component={SignUp}></Route>
-    </div>
-  );
+  logout = event => {
+    event.preventDefault()
+    localStorage.removeItem('jwt')
+    this.props.history.push('/login')
+  }
 }
 
 export default withRouter(App);
--- auth-app/src/components/Login.js
+++ auth-app/src/components/Login.js
@@ -1,42 +1,55 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 export default class Login extends React.Component {
-    state = {
-        username: '',
-        password: ''
-    }
+  state = {
+    username: "",
+    password: ""
+  };
 
-    render() {
-        return(
-            <div>
-                <h1>Log In</h1>
-                <form onSubmit={this.login}>
-                    <input required placeholder='username' name='username' value={this.state.username} onChange={this.handleChanges}></input>
-                    <input required placeholder='password' name='password' value={this.state.password} onChange={this.handleChanges}></input>
-                    <button>Log In</button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='form-container'>
+        <h1>Log In</h1>
+        <form className='form' onSubmit={this.login}>
+          <input
+            required
+            placeholder="username"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChanges}
+          />
+          <input
+            required
+            placeholder="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChanges}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
 
-    handleChanges = event => {
-        this.setState({ 
-            ...this.state,
-            [event.target.name]: event.target.value
-        })
-    }
+  handleChanges = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  };
 
-    login = event => {
-        event.preventDefault()
-        const endpoint = `http://localhost:3300/api/login`
-        axios.post(endpoint, this.state)
-        .then(res => {
-            localStorage.setItem('jwt', res.data.token)
-            this.props.history.push('/jokes')
-        })
-        .catch(err => {
-            console.error(err)
-        })
-    }
+  login = event => {
+    event.preventDefault();
+    const endpoint = `http://localhost:3300/api/login`;
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        localStorage.setItem("jwt", res.data.token);
+        this.props.history.push("/jokes");
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 }
