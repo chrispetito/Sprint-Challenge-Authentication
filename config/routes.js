ac82c1722cc252e361a1f0c699cb6571a246e1d6
chrispetito <christopherpetito@gmail.com>
2019-06-14 08:20:01 -0700

    implement user registration

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs')
 
 const { authenticate } = require('../auth/authenticate');
+const users = require('./model')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,6 +12,15 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 8)
+  user.password = hash 
+  users.add(user).then(user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 }
 
 function login(req, res) {
